f66fedd02d63c155f088e67f67f12f874069aa84
balmed <iwymed@gamil.com>
2023-10-18 04:33:05 -0700

    crate file free_fupr

--- funct_proced.c
+++ funct_proced.c
@@ -1,24 +1,5 @@
 #include "shell.h"
 /**
- * free_array - free array
- * @array: Arguments string
- *
- * Return: Always 0 (Success)
- */
-void free_array(char **array)
-{
-	int i;
-
-	if (!array)
-		return;
-
-	for (i = 0 ; array[i] != NULL ; i++)
-	{
-		free(array[i]), array[i] = NULL;
-	}
-	free(array), array = NULL;
-}
-/**
  * _strdup - The duplicate the string.
  * @str: input string.
  *
